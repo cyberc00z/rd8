e691d0afcaff09af52855fe9d4903a4a9fcb103b
xadhrit <maharajrabel@gmail.com>
2021-05-06 04:26:48 -0400

    about page

---
+++ src/pages/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+import SEO from "../components/seo";
+import Bio from "../components/bio";
+import Layout from "../components/layout";
+
+
+const About = ({data, location}) => {
+
+	const siteTitle = data.site.siteMetadata?.title || `Title`
+
+	return (
+	
+		<Layout={location} title={siteTitle}>
+		  <SEO title="About" />
+		  <Link to="/about" />
+		  <Bio />
+		</Layout>
+	)
+
+}
+
+export default About;
+
+
