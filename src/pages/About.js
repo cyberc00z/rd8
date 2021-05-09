ece28fde461c51ba7bc309f18ce549d1897c92ba
xadhrit <maharajrabel@gmail.com>
2021-05-09 00:33:44 -0400

    mine bio

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,7 @@ import React from "react";
 import SEO from "../components/seo";
 import Layout from "../components/layout";
 import {Link, graphql} from "gatsby"
+import './style.css';
 
 const About = ({data, location}) => {
 
