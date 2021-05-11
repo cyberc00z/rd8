0e0652df497dd253c3575bee08ef31751a984ba5
xadhrit <maharajrabel@gmail.com>
2021-05-11 00:26:42 -0400

    home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
-import Bio from "../components/bio"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import "./style.css"
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Bio from "../components/bio";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
