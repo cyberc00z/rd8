e0c8667d8d92c582674577708418a5f1eb869aaa
xadhrit <maharajrabel@gmail.com>
2021-05-11 00:46:12 -0400

    not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,7 @@ import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import './style.css'
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
@@ -10,9 +11,10 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p> I think you are searching for something else May be for... the sadness.</p>
-      <img src={ require('./404.png')} alt="404" />
+      <div className="not_found">
+      <h1>404: Not Found</h1>          
+      <p> I think you are searching for something else May be for... the sadness. </p>
+    </div>
     </Layout>
   )
 }
