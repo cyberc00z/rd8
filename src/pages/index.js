f23508add01322b671509005f5320be1e0be2cb3
xadhrit <maharajrabel@gmail.com>
2023-12-18 20:12:08 +0530

    music

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import SEO from "../components/seo";
 import Layout from "../components/layout";
 import {graphql} from "gatsby"
 import './style.css';
 
+// add music
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import retroMusic from "../../content/assets/sm.mp3";
+import {faPlay, faPause} from "@fortawesome/free-solid-svg-icons";
 
-const index = ({data, location}) => {
 
+
+const Index = ({data, location}) => {
 	const siteTitle = data.site.siteMetadata?.title || `Title`;
+        
+        const [isPlaying, setIsPlaying] = useState(false)
+        
+        const audioRef = React.createRef();
+        useEffect(() => {
+           const audioElement = audioRef.current;
+           if (isPlaying){
+           audioElement.play()
+           } else {
+              audioElement.pause();
+           }
+       });
+	
 	
 	return (
 		<Layout location={location} title={siteTitle}>
@@ -31,13 +49,27 @@ const index = ({data, location}) => {
            <br />
 		   4. <a href="mailto:adhirt77@gmail.com">adhirt77@gmail.com</a>
        </p>
+		<div className="musicControls">
+		   <img src="https://t2.genius.com/unsafe/252x252/https%3A%2F%2Fimages.genius.com%2Ff89691712e7b01c1ac87b185c1efa724.1000x1000x1.jpg" alt="retro" />
+		    <div className="playbar">
+                <button className={isPlaying ? "Playing" : ""} onClick={() => setIsPlaying(!isPlaying)}>
+                   <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
+                </button>
+            <audio ref={audioRef}>
+               <source src={retroMusic} type="audio/mp3" />
+            </audio>
+            <p>Namstatue</p>
+        </div>
+		   
+		    </div>
 		  </div>		  
+		  
 		</Layout>
 	)
 
 }
 
-export default index;
+export default Index;
 
 export const pageQuery = graphql`
   query {
