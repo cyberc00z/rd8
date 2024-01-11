7f872f248d7c9b20e3aca63b7bbf67e59891144a
xadhrit <maharajrabel@gmail.com>
2024-01-11 19:20:33 +0530

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,6 +78,7 @@ const Index = ({ data, location }) => {
           <br />
         </p>
 
+         <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/664637079&color=%235c4c3c&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/azadirecords" title="Azadi Records" target="_blank" style="color: #cccccc; text-decoration: none;">Azadi Records</a> · <a href="https://soundcloud.com/azadirecords/sets/bayaan-seedhe-maut-azadi-records-azr010" title="&#x27;Bayaan&#x27;  | Seedhe Maut | Azadi Records | AZR010" target="_blank" style="color: #cccccc; text-decoration: none;">&#x27;Bayaan&#x27;  | Seedhe Maut | </a></div> 
         <br />
         <p>Your Location : SV'80 lunar city, Moon.</p>
       </div>
